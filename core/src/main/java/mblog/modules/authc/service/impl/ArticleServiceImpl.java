ecc2c1e86cec27c899f9d2863e3afb19cd064cb6
dianyu <527088995@qq.com>
2019-01-23 16:43:29 +0800

    爬取数据增加查询OSC页数

--- core/src/main/java/mblog/modules/authc/service/impl/ArticleServiceImpl.java
+++ core/src/main/java/mblog/modules/authc/service/impl/ArticleServiceImpl.java
@@ -33,12 +33,17 @@ public class ArticleServiceImpl implements ArticleService {
     //@Transactional
     public long save(){
         try {
-            this.searchCsdnUrl();
+            this.saveOscPage2();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return 1;
     }
+
+    /**
+     * 保存CSDN博客内容详情
+     * @return
+     */
     @Override
     @Transactional
     public long saveMpost(){
@@ -88,6 +93,11 @@ public class ArticleServiceImpl implements ArticleService {
 
         return html;
     }
+
+    /**
+     * 保存OSC博客内容详情
+     * @return
+     */
     @Override
     @Transactional
     public long saveOsc(){
@@ -294,6 +304,112 @@ public class ArticleServiceImpl implements ArticleService {
                 }
                 System.out.println("文章总数++=:" + articleEntity.getReadNum());
                 System.out.println("文章总数++=:" + articleEntity.getAddress());
+                System.out.println("文章类型+++:" + articleEntity.getType());
+            }
+            //遍历输出ArrayList里面的爬取到的文章
+            System.out.println("文章总数:" + articleList.size());
+            System.out.println("文章绝对路劲地址:oooooooooooooooo++++++++++++++++++++++++++++++");
+        }
+    }
+
+    /**
+     * 保存第二页数据
+     * @throws IOException
+     */
+    public  void saveOscPage2() throws IOException {
+        List<String> urlList=new ArrayList<>();
+        urlList.add("5611447");//ai
+        urlList.add("5593654");//cloud
+        urlList.add("428639");//cloud
+        urlList.add("5765988");//blockchain
+        urlList.add("428602");//mobile
+        urlList.add("428612");//web
+        urlList.add("428640");//engineering
+        urlList.add("429511");//game
+        urlList.add("428609");//lang
+        urlList.add("428610");//db
+        urlList.add("428613");//ops
+        urlList.add("6289115");//iot
+        urlList.add("428647");//avi
+        urlList.add("430381");//other
+        urlList.add("428638");//arch
+        for(String type:urlList){
+            String url="https://www.oschina.net/blog?classification=";
+            String relUrl=url+type;
+            ///获取url地址的http链接Connection
+            Connection conn1 = Jsoup.connect(relUrl)	//博客首页的url地址
+                    .userAgent("Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10")	//http请求的浏览器设置
+                    .timeout(1000)   //http连接时长
+                    .method(Connection.Method.GET);  //请求类型是get请求，http请求还是post,delete等方式
+            //获取页面的html文档
+            Document doc1 = conn1.get();
+            //获取url地址的http链接Connection
+            String page2Url="https://www.oschina.net/blog/widgets/_blog_index_recommend_list?classification="+type+"&p=7&type=ajax";
+            Connection conn = Jsoup.connect(page2Url)	//博客首页的url地址
+                    .userAgent("Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10")	//http请求的浏览器设置
+                    .timeout(1000)   //http连接时长
+                    .method(Connection.Method.GET);  //请求类型是get请求，http请求还是post,delete等方式
+            //获取页面的html文档
+            Document doc = conn.get();
+            Element body = doc.body();
+
+            Elements articleList = body.getElementsByClass("content");
+            for(Element article : articleList){
+                Article articleEntity = new Article();
+                //标题
+                Element linkNode = (article.select("div a")).get(0);
+                //文章简介
+                Element desptionNode = (article.getElementsByClass("line-clamp")).get(0);
+                //Element articleManageNode = (article.getElementsByClass("time")).get(0);
+                String readNum = (article.getElementsByClass("item")).get(2).text();
+                Element commentNum = (article.getElementsByClass("item")).get(4);
+
+                articleEntity.setAddress(linkNode.attr("href"));
+                articleEntity.setTitle(linkNode.text());
+                articleEntity.setDesption(desptionNode.text());
+                // articleEntity.setTime(articleManageNode.select("span:eq(0").text());
+                //阅读量
+                if("".equals(readNum)){
+                    articleEntity.setReadNum(0);
+                }else {
+                    if(readNum.contains("K")){
+                        String rNum=readNum.replaceAll("K","");
+                        float redNum=Float.parseFloat(rNum);
+                        double readNums=redNum*1000;
+                        articleEntity.setReadNum((int)readNums);
+                    }else {
+                        articleEntity.setReadNum(Integer.valueOf(readNum));
+                    }
+                }
+                //评论
+                if("".equals(commentNum.text())){
+                    articleEntity.setCommentNum(0);
+                }else {
+                    articleEntity.setCommentNum(Integer.parseInt(commentNum.text()));
+                }
+                if("5611447".equals(type)){articleEntity.setType("ai");}
+                if("5593654".equals(type)){articleEntity.setType("cloud");}
+                if("428639".equals(type)){articleEntity.setType("cloud");}
+                if("5765988".equals(type)){articleEntity.setType("blockchain");}
+                if("428602".equals(type)){articleEntity.setType("mobile");}
+                if("428612".equals(type)){articleEntity.setType("web");}
+                if("428640".equals(type)){articleEntity.setType("engineering");}
+                if("429511".equals(type)){articleEntity.setType("game");}
+                if("428609".equals(type)){articleEntity.setType("lang");}
+                if("428610".equals(type)){articleEntity.setType("db");}
+                if("428613".equals(type)){articleEntity.setType("ops");}
+                if("6289115".equals(type)){articleEntity.setType("iot");}
+                if("428647".equals(type)){articleEntity.setType("avi");}
+                if("430381".equals(type)){articleEntity.setType("other");}
+                if("428638".equals(type)){articleEntity.setType("arch");}
+                articleEntity.setTime(new Date());
+                articleEntity.setBlogType("OSC");
+                articleEntity.setStatus(0);
+                if(articleEntity.getReadNum()>300){
+                    articleDao.save(articleEntity);
+                }
+                System.out.println("文章阅读数++=:" + articleEntity.getReadNum());
+                System.out.println("文章地址++=:" + articleEntity.getAddress());
                 System.out.println("文章类型+++:" + articleEntity.getType());
             }
             //遍历输出ArrayList里面的爬取到的文章
