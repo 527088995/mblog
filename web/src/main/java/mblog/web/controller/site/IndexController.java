664b3a2e802e6d895f08d75f21e3590ecb4317d9
minHOu <527088995@qq.com>
2019-01-25 23:51:08 +0800

    实现按照类型查询

--- web/src/main/java/mblog/web/controller/site/IndexController.java
+++ web/src/main/java/mblog/web/controller/site/IndexController.java
@@ -11,6 +11,7 @@ package mblog.web.controller.site;
 
 import javax.servlet.http.HttpServletRequest;
 
+import mblog.modules.blog.entity.Channel;
 import mblog.modules.blog.service.ArticleTypeService;
 import mblog.modules.blog.service.BlogClassService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mblog.base.lang.Consts;
@@ -49,4 +51,27 @@ public class IndexController extends BaseController{
 		return view(Views.INDEX);
 	}
 
+	/**
+	 * 根据文章类型查看
+	 * @param
+	 * @return
+	 */
+	@RequestMapping("/blogClass/{type}")
+	public String searchByBlogClass(@PathVariable String type, ModelMap model, HttpServletRequest request) {
+		// init params
+		String order = ServletRequestUtils.getStringParameter(request, "order", Consts.order.NEWEST);
+		int pn = ServletRequestUtils.getIntParameter(request, "pn", 1);
+		String blogClass=request.getParameter("blogClass");//匹配博客分类
+
+		Channel channel = null;
+		// callback params
+		model.put("channel", channel);
+		model.put("order", order);
+		model.put("pn", pn);
+		model.put("blogClass", type);//匹配博客分类
+		model.put("blogClasss", blogClassService.findAll(Consts.STATUS_NORMAL));//博客分类
+		model.put("articleTypes", articleTypeService.findAll(Consts.STATUS_NORMAL));
+
+		return view(Views.INDEX);
+	}
 }
